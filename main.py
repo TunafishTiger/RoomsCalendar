c833168e87e62d763caede80dcbdb76d6c31203a
Sean Gibson <seangibsonbooks@outlook.com>
2022-03-15 06:03:13 -0400

    Refactored to fewer lines of code by defining functions.

--- main.py
+++ main.py
@@ -32,6 +32,19 @@ def main():
     )
     console.print("\n")
 
+    # Declare function to imprint closure.
+    def closed(img_to_close):
+        img_to_close = img_to_close
+        img_to_close.paste(img_closed, (0, 0), mask=img_closed)
+        img_to_close.save(sheet_name, format="png")
+
+    # Declare function to imprint holiday inserts.
+    def insert(img_to_overlay, holiday_insert):
+        img_to_overlay = img_to_overlay
+        holiday_insert = holiday_insert
+        img_to_overlay.paste(holiday_insert, (0, 0), mask=holiday_insert)
+        img_to_overlay.save(sheet_name, format="png")
+
     # Begin 1 infinite loop.
     while True:
         try:
@@ -90,7 +103,6 @@ def main():
             img_sun = Image.open("3_RoomSchedule_Template_Sundays.png").convert("RGB")
 
             # Define artwork for all of our observed holidays and special closures.
-            no_holiday = Image.open("holidays/Blank.png").convert("RGBA")
             new_years_day = Image.open("holidays/NewYearsDay.png").convert("RGBA")
             mlk_day = Image.open("holidays/MLKDay.png").convert("RGBA")
             valentines_day = Image.open("holidays/ValentinesDay.png").convert("RGBA")
@@ -122,9 +134,6 @@ def main():
                 match single_date.weekday():
                     case 6:
                         img_in_memory = img_sun.copy()
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
                     case 5:
                         img_in_memory = img_sat.copy()
                     case 4:
@@ -149,111 +158,68 @@ def main():
                     font=yf,
                 )
 
-                holiday_insert = no_holiday
-
                 # Account manually each year for long weekends around holidays or for in-staff days.
                 match datetime.strftime(single_date, "%Y-%m-%d"):
                     case "2022-02-14":  # Valentine's Day.
-                        holiday_insert = valentines_day.copy()
+                        insert(img_in_memory, valentines_day.copy())
                     case "2022-04-16":  # Good Friday weekend.
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
                     case "2022-05-28":  # Memorial Day weekend.
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
                     case "2022-09-03" | "2022-09-05":  # Labor Day weekend.
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
                     case "2022-10-31":  # Halloween.
-                        holiday_insert = halloween.copy()
+                        insert(img_in_memory, halloween.copy())
                     case "2022-11-25" | "2022-11-26":  # Thanksgiving Day weekend.
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
                     case "2022-12-23" | "2022-12-24" | "2022-12-26":  # Christmas weekend.
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
 
                 # Account for holidays based on algorithm, never needing updating.
                 # 'Observed' days do not get holiday inserts, only closure status.
                 match us_holidays.get(f"{single_date}"):
                     case "New Year's Day":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = new_years_day.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, new_years_day.copy())
                     case "New Year's Day (Observed)":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
                     case "Martin Luther King Jr. Day":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = mlk_day.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, mlk_day.copy())
                     case "Good Friday":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = good_friday.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, good_friday.copy())
                     case "Memorial Day":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = memorial_day.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, memorial_day.copy())
                     case "Independence Day":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = independence_day.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, independence_day.copy())
                     case "Independence Day (Observed)":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
                     case "Labor Day":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = labor_day.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, labor_day.copy())
                     case "Veterans Day":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = veterans_day.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, veterans_day.copy())
                     case "Veterans Day (Observed)":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
                     case "Thanksgiving":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = thanksgiving.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, thanksgiving.copy())
                     case "Christmas Eve":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = christmas_eve.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, christmas_eve.copy())
                     case "Christmas Eve (Observed)":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
+                        closed(img_in_memory)
                     case "Christmas Day":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = christmas_day.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, christmas_day.copy())
                     case "New Year's Eve":
-                        img_in_memory.paste(img_closed, (0, 0), mask=img_closed)
-                        img_in_memory.save(sheet_name, format="png")
-                        img_in_memory = Image.open(sheet_name)
-                        holiday_insert = new_years_eve.copy()
+                        closed(img_in_memory)
+                        insert(img_in_memory, new_years_eve.copy())
 
-                img_in_memory.paste(holiday_insert, (0, 0), mask=holiday_insert)
                 img_in_memory.save(sheet_name, format="png")
 
                 # We use CUPS for printing, which should be available for all UNIX-type systems.
