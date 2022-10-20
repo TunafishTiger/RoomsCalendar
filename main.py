42487f6e8039d432c009089bef7bc1bcfe9bae30
Sean Gibson <seangibsonbooks@outlook.com>
2022-10-19 20:40:25 -0400

    Tidy up.

--- main.py
+++ main.py
@@ -18,53 +18,54 @@ from rich.prompt import Prompt
 from sh import lpr
 
 
-class Calendar:
-    #  Define basic elements to construct our calendar.
-    STATUS_CLOSED: Final = Image.open("4_Asset_ClosedToday.png").convert("RGBA")
-    WEEKDAY_HOURS: Final = Image.open("0_Asset_WeekdayHours.png").convert("RGB")
-    FRIDAY_HOURS: Final = Image.open("1_Asset_FridayHours.png").convert("RGB")
-    SATURDAY_HOURS: Final = Image.open("2_Asset_SaturdayHours.png").convert("RGB")
-    SATURDAY_HOURS_EXTENDED: Final = Image.open(
-        "2_Asset_SaturdayHours_Extended.png"
-    ).convert("RGB")
-    SUNDAY_HOURS: Final = Image.open("3_Asset_SundayHours.png").convert("RGB")
-    SUNDAY_HOURS_EXTENDED: Final = Image.open("3_Asset_SundayHours_Extended.png").convert(
-        "RGB"
-    )
-
-    #  Define our fonts and sizes.
-    WEEKDAYNAME_FONT: Final = ImageFont.truetype("SF-Pro-Text-Black.ttf", 160)
-    DATESTAMP_FONT: Final = ImageFont.truetype("SF-Pro-Text-Black.ttf", 124)
-
-
-class Art:
-    #  Define artwork that can be overlayed.
-    NEWYEARSDAY: Final = Image.open("art/NewYearsDay.png").convert("RGBA")
-    MLKDAY: Final = Image.open("art/MLKDay.png").convert("RGBA")
-    VALENTINESDAY: Final = Image.open("art/ValentinesDay.png").convert("RGBA")
-    GOODFRIDAY: Final = Image.open("art/GoodFriday.png").convert("RGBA")
-    MEMORIALDAY: Final = Image.open("art/MemorialDay.png").convert("RGBA")
-    JUNETEENTH: Final = Image.open("art/Juneteenth.png").convert("RGBA")
-    INDEPENDENCEDAY: Final = Image.open("art/IndependenceDay.png").convert("RGBA")
-    LABORDAY: Final = Image.open("art/LaborDay.png").convert("RGBA")
-    VETERANSDAY: Final = Image.open("art/VeteransDay.png").convert("RGBA")
-    HALLOWEEN: Final = Image.open("art/Halloween.png").convert("RGBA")
-    THANKSGIVING: Final = Image.open("art/Thanksgiving.png").convert("RGBA")
-    CHRISTMASEVE: Final = Image.open("art/ChristmasEve.png").convert("RGBA")
-    CHRISTMASDAY: Final = Image.open("art/ChristmasDay.png").convert("RGBA")
-    NEWYEARSEVE: Final = Image.open("art/NewYearsEve.png").convert("RGBA")
-
-
 def main():
+
+    class Calendar:
+        #  Define basic elements to construct our calendar.
+        STATUS_CLOSED: Final = Image.open("4_Asset_ClosedToday.png").convert("RGBA")
+        WEEKDAY_HOURS: Final = Image.open("0_Asset_WeekdayHours.png").convert("RGB")
+        FRIDAY_HOURS: Final = Image.open("1_Asset_FridayHours.png").convert("RGB")
+        SATURDAY_HOURS: Final = Image.open("2_Asset_SaturdayHours.png").convert("RGB")
+        SATURDAY_HOURS_EXTENDED: Final = Image.open(
+            "2_Asset_SaturdayHours_Extended.png"
+        ).convert("RGB")
+        SUNDAY_HOURS: Final = Image.open("3_Asset_SundayHours.png").convert("RGB")
+        SUNDAY_HOURS_EXTENDED: Final = Image.open("3_Asset_SundayHours_Extended.png").convert(
+            "RGB"
+        )
+
+        #  Define our fonts and sizes.
+        WEEKDAYNAME_FONT: Final = ImageFont.truetype("SF-Pro-Text-Black.ttf", 160)
+        DATESTAMP_FONT: Final = ImageFont.truetype("SF-Pro-Text-Black.ttf", 124)
+
+
+    class Art:
+        #  Define artwork that can be overlayed.
+        NEWYEARSDAY: Final = Image.open("art/NewYearsDay.png").convert("RGBA")
+        MLKDAY: Final = Image.open("art/MLKDay.png").convert("RGBA")
+        VALENTINESDAY: Final = Image.open("art/ValentinesDay.png").convert("RGBA")
+        GOODFRIDAY: Final = Image.open("art/GoodFriday.png").convert("RGBA")
+        MEMORIALDAY: Final = Image.open("art/MemorialDay.png").convert("RGBA")
+        JUNETEENTH: Final = Image.open("art/Juneteenth.png").convert("RGBA")
+        INDEPENDENCEDAY: Final = Image.open("art/IndependenceDay.png").convert("RGBA")
+        LABORDAY: Final = Image.open("art/LaborDay.png").convert("RGBA")
+        VETERANSDAY: Final = Image.open("art/VeteransDay.png").convert("RGBA")
+        HALLOWEEN: Final = Image.open("art/Halloween.png").convert("RGBA")
+        THANKSGIVING: Final = Image.open("art/Thanksgiving.png").convert("RGBA")
+        CHRISTMASEVE: Final = Image.open("art/ChristmasEve.png").convert("RGBA")
+        CHRISTMASDAY: Final = Image.open("art/ChristmasDay.png").convert("RGBA")
+        NEWYEARSEVE: Final = Image.open("art/NewYearsEve.png").convert("RGBA")
+
+
     #  Declare helper function to imprint closure.
     def overlay_closed_status():
         calendar_sheet.paste(Calendar.STATUS_CLOSED, (0, 0), mask=Calendar.STATUS_CLOSED)
-        calendar_sheet.save(calendar_sheet_name, format="png")
+        calendar_sheet.save(calendar_sheet_filename, format="png")
 
     #  Declare helper function to imprint holiday inserts.
     def overlay_artwork(art_to_use):
         calendar_sheet.paste(art_to_use, (0, 0), mask=art_to_use)
-        calendar_sheet.save(calendar_sheet_name, format="png")
+        calendar_sheet.save(calendar_sheet_filename, format="png")
 
     #  Compute deltas. Wrap iteration in console UI output.
     def daterange_to_print(first_date, last_date):
@@ -137,7 +138,7 @@ def main():
             ):
 
                 #  Define a filename scheme.
-                calendar_sheet_name = single_date.strftime(
+                calendar_sheet_filename = single_date.strftime(
                     "pages/Calendar %A %b %d %Y.pdf"
                 )
 
@@ -247,11 +248,11 @@ def main():
                         overlay_artwork(Art.NEWYEARSEVE)
 
                 #  Save our transformed calendar page onto the filesystem.
-                calendar_sheet.save(calendar_sheet_name, format="pdf")
+                calendar_sheet.save(calendar_sheet_filename, format="pdf")
 
                 #  At the end of each loop:
-                #  append the file we've just saved into a multi-page PDF.
-                merger.append(calendar_sheet_name)
+                #  append the file we've just saved into a multipage PDF.
+                merger.append(calendar_sheet_filename)
 
             #  Derive a filename for our new multi-page PDF file.
             calendar_month_name = f"{answer}_{year_we_want_to_print_for}"
